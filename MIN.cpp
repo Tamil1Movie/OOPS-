11f41857358548a2d12c7ddc5d32f68e90a590e7
DM <167101844+Tamil1Movie@users.noreply.github.com>
2024-10-26 17:12:44 +0530

    Create MIN.cpp

---
+++ MIN.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class minimum
+{
+    public:
+          void min(int a,int b, int c)
+            {
+                if(a<b&&a<c)
+                    cout<<a<<" is minimum"<<endl;
+                else if(b<c)
+                    cout<<b<<" is minimum"<<endl;
+                else 
+                    cout<<c<<" is minimum"<<endl;
+            }
+            void min(double x,double y, double z)
+            {
+                if(x<y&&x<z)
+                    cout<<x<<" is minimum"<<endl;
+                else if(y<z)
+                    cout<<y<<" is minimum"<<endl;
+                else 
+                    cout<<z<<" is minimum"<<endl;
+            }
+};
+int main()
+{
+    minimum m;
+    m.min(16,88,50);
+    m.min(21.5,46.7,18.7);
+    return 0;
+}
